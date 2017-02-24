dfe3f70ceb4cc4880cf2351fc861332de1739644
Basti Tee <basti.tee@gmx.de>
2017-02-24 11:36:34 +0100

    smooth;

--- _includes/script.js
+++ _includes/script.js
@@ -1,8 +1,6 @@
-$(document).ready(function() {
-    $('#main-body').addClass('animated fadeIn');
-    $('#site-title').addClass('animated zoomIn');
-    $('#site-avatar').addClass('animated zoomIn');
-    $('#site-bloglist').addClass('animated slideInLeft');
-    $('#site-content').addClass('animated slideInRight');
-    $('#site-logo').addClass('animated slideInDown');
-});
+$('#main-body').addClass('animated fadeIn');
+$('#site-title').addClass('animated zoomIn');
+$('#site-avatar').addClass('animated zoomIn');
+$('#site-bloglist').addClass('animated slideInLeft');
+$('#site-content').addClass('animated slideInRight');
+$('#site-logo').addClass('animated slideInDown');
